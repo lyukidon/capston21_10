8526e22b8d9473aa83af0b1657f2ee18e66aecb8
lyukidon <qgx99098@gmail.com>
2021-10-06 20:13:16 +0900

    빌딩부분 오류 수정

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ document.querySelector('button').addEventListener('click',function(){
     document.querySelector('th#Bw').innerHTML = Math.round(0.4 * lr * 10000) / 10000
     document.querySelector('th#bh').innerHTML = Math.round(0.4 * lr * 10000) / 10000
     document.querySelector('th#bl').innerHTML = Math.round(0.4 * lr * 10000) / 10000
-    document.querySelector('th#s').innerHTML = `${Math.round(10000 * 1.0*lr)/10000} / ${Math.round(10000 * 9.8*lr)/10000}`
+    document.querySelector('th#s').innerHTML = `${Math.round(10000 * 1.0*lr)/10000} / ${Math.round(10000 * 0.8*lr)/10000}`
     document.querySelector('th#wD').innerHTML = Math.round(0.98 * lr * 10000) / 10000
     document.querySelector('th#amp').innerHTML = Math.round(0.208 * lr * 10000) / 10000
     document.querySelector('th#tS').innerHTML = Math.round(5.69 * (lr**(1/2)) * 10000) / 10000
     document.querySelector('th#exp').innerHTML = Math.round(41.7 * lr * 10000) / 10000
     document.querySelector('th#slpLength').innerHTML = `밑변:${Math.round(10000 * 20*lr)/10000} 높이:${Math.round(10000 * 1*lr)/10000} 빗변:${Math.round(10000 * Math.sqrt(401)*lr)/10000}`
+    console.log(1*lr)
 })
